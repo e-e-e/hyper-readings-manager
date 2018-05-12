c603676367f4f5bf8b462736f99cafbafcfc372b
Benjamin Forster <benjamin.forster@loanmarket.com.au>
2018-05-12 10:58:30 +0700

    update rollup syntax

--- rollup.config.js
+++ rollup.config.js
@@ -4,17 +4,12 @@ import babel from 'rollup-plugin-babel'
 import pkg from './package.json'
 
 export default [
-  // CommonJS (for Node) and ES module (for bundlers) build.
-  // (We could have three entries in the configuration array
-  // instead of two, but it's quicker to generate multiple
-  // builds from a single configuration where possible, using
-  // the `targets` option which can specify `dest` and `format`)
   {
     input: 'src/manager.js',
-    external: [...Object.keys(pkg.dependencies)],
-    targets: [
-      { dest: pkg.main, format: 'cjs' },
-      { dest: pkg.module, format: 'es' }
+    external: [...Object.keys(pkg.dependencies), 'fs', 'events', 'path'],
+    output: [
+      { file: pkg.main, format: 'cjs' },
+      { file: pkg.module, format: 'es' }
     ],
     plugins: [
       babel({
